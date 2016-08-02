0343fb69e179df785402722071012125fdaad709
Kuriyama <paprapapra@gmail.com>
2016-08-02 18:30:43 +0900

    Add some module(magit, bind-key), configure magit.

--- init.el
+++ init.el
@@ -14,28 +14,44 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
+;;
 ;; Config install-package(for el-get)
+;;
 
 ;; Common
 (el-get-bundle auto-complete)
 (el-get-bundle helm)
 (el-get-bundle quickrun)
+(el-get-bundle bind-key)
 
-;; For Swift
+;; Swift
 (el-get-bundle swift-mode)
 
-;; For markdown
+;; Mmarkdown
 (el-get-bundle markdown-mode)
 
-;; For TypeScript
+;; TypeScript
 (el-get-bundle typescript-mode)
 
-;; For Rust
+;; Rust
 (el-get-bundle rust-mode)
 
-;; For ld script
+;; ld script
 ;;(el-get-bundle ld-mode :type git :url "https://github.com/spenczar/ld-mode.git")
 (el-get-bundle ld-mode :type http :url "http://opensource.apple.com//source/emacs/emacs-84/emacs/lisp/progmodes/ld-script.el?txt")
 
-;; For erlang
+;; erlang
 (el-get-bundle erlang-mode)
+
+;; magit
+(el-get-bundle magit)
+
+;;
+;; For magit config
+;;
+
+(setq-default magit-auto-revert-mode nil)
+(setq vc-handled-backends '())
+(eval-after-load "vc" '(remove-hook 'find-file-hooks 'vc-find-file-hook))
+(bind-key "C-x m" 'magit-status)
+(bind-key "C-c l" 'magit-blame)
